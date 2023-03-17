21247547465ecc887272937e90cc0a700c58e6be
walid <jabiriwalid555@gmail.com>
2023-03-17 07:16:31 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	prinf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
-	prinf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
-	prinf("Size of a long int: %lu.\n", (unsigned long)sizeof(li));
-	prinf("Size of a long long int: %lu.\n", (unsigned long)sizeof(lli));
-	prinf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
+	prinf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	prinf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	prinf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	prinf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	prinf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return(0);
 }
