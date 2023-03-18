7e41d03ee98a3ba6ff4c46b5ff1c52df779d618b
walid <jabiriwalid555@gmail.com>
2023-03-17 17:53:05 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Printing numbers from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 38; i < 48; i++)
+	{
+		putchar(i);
+	}
+	for (i = 87; i < 93; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
