359b4c03097a68fa49b143104ab6de875b29b3fb
walid <jabiriwalid555@gmail.com>
2023-03-20 07:28:50 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		if (i != 57)
+		if (i != '9')
 		{
 			putchar(',');
 			putchar(' ');
+		}
+		else if (i == '9')
+		{
+			break;
 		}
 	}
 	putchar('\n');
