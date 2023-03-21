e5e11c55de3039dc53634db45be3784758d08279
walid <jabiriwalid555@gmail.com>
2023-03-21 12:56:30 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Void
  */
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a';c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
