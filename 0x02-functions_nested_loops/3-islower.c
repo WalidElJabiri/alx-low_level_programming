3f36016c9ed697092e7f0ea02370a611d0450a68
walid <jabiriwalid555@gmail.com>
2023-03-21 14:33:41 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 
 /**
  * _islower - check if int is lower
- *
+ * @c: the int that will be checked
  * Return: 1 if lower, else 0
  */
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
