59c20d21a9093708873c34980218136bc71e7fa8
walid <jabiriwalid555@gmail.com>
2023-03-21 12:52:14 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a';c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,2 @@
 int _putchar(char c);
-void alphabet(void);
-void jack_daniel(void);
-void print_num(int n);
-int add(int, int);
+void print_alphabet(void);
