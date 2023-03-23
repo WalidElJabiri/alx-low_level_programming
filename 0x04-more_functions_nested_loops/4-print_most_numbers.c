d36f71d304b73dd9c95ce5561d67c0284adc2f70
walid <jabiriwalid555@gmail.com>
2023-03-23 09:24:54 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,9 @@ void print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i != 50 || i != 52)
+		while (i != 50 && i != 52)
 		{
-			continue;
-		}
-		else
-		{
-			_putchar(i + '0');
+			_putchar(i);
 		}
 	}
 	_putchar('\n');
