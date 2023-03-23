40a321e413bd777b10a3ae0505b355b3a3672272
walid <jabiriwalid555@gmail.com>
2023-03-23 08:46:36 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * @i: the num
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+		_putchar('\n');
+	}
+}
