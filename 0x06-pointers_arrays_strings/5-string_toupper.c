721584a8f036d6c6b179501aa2b46176dd9795be
walid <jabiriwalid555@gmail.com>
2023-03-30 16:00:51 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: the string
+ * Return: str
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
