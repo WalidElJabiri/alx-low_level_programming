89e9da516134a4b5bfe8e0d9d36adec9d41772ac
walid <jabiriwalid555@gmail.com>
2023-04-05 19:21:26 -0700

    C - Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * pr - the helping func
+ * @n: the num
+ * @i: i times
+ * Return:1 if prime, 0 if not
+ */
+int pr(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if ((n / i) < i)
+	{
+		return (1);
+	}
+	if (n % i == 0 && i > 1)
+	{
+		return (0);
+	}
+	return (pr(n, i + 1));
+}
+/**
+ * is_prime_number -  returns 1 if the input integer is prime otherwise return 0
+ * @n: the num
+ * Return: 1 if the input integer is prime otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	return (pr(n,1));
+}
