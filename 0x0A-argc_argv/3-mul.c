48f7436694135fd1e48208df7c4696f076dba2ab
walid <jabiriwalid555@gmail.com>
2023-04-10 10:09:25 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: argument count
+ * @argv: vector of C strings
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	if (argc > 1 && argc < 4)
+	{
+		int x = atoi(argv[1]), y = atoi(argv[2]);
+
+		int sum = x * y;
+
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
