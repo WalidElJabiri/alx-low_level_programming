5b3461df4ba9ce6575b8ab909fa692429d7f80a5
walid <jabiriwalid555@gmail.com>
2023-04-17 14:35:21 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef dog
+#define dog
+/**
+ * struct dog - a structure called dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
