3d6e3a9530b671804f1c88ca4a15ff87d03521ec
walid <jabiriwalid555@gmail.com>
2023-05-02 09:19:04 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a listint_t list
+ * @head: ptr to the head of the list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *l;
+	while (head)
+	{
+		l = head->next;
+		free(head);
+		head = l;
+	}
+}
