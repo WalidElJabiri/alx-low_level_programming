cc610be2e232418e00c29ab3bed8d0e742639947
walid <jabiriwalid555@.com>
2023-06-16 21:18:47 +0000

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * dlistint_len - returns the number of elements in a linked list.
- * @dlistint_t: doubly linked list
+ * print_dlistint - returns the number of elements in a linked list.
  * @h: head
  * Return: num of elements
  */
-size_t print_dlistint(const dlistint_t *h){
-        int i = 0;
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
 
-        if (h == NULL)
-                return 0;
-        else
-        {
-            while (h->prev != NULL)
-            {
-                h = h->prev;
-            }
-            while (h != NULL)
-            {
-                printf("%d\n", h->n);
-                h = h->next;
-                i++;
-                
-            }
-            return i;
-        }
+	if (h == NULL)
+		return 0;
+	else
+	{
+		while (h->prev != NULL)
+		{
+			h = h->prev;
+		}
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
+		return i;
+	}
 }
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+/**
+ * dlistint_len - returns the number of elements in a linked list.
+ * @h: head
+ * Return: num of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+        int i = 0;
+
+        if (h == NULL)
+                return 0;
+        else
+        {
+                while (h->prev != NULL)
+                {
+                        h = h->prev;
+                }
+                while (h != NULL)
+                {
+                        h = h->next;
+                        i++;
+                }
+                return i;
+        }
+}
